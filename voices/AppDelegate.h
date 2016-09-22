870c345a5e8b9e38d65def92d7924ded4cbb27d6
mac <mac@macdeimac.local>
2016-09-22 23:04:28 +0800

    Initial Commit

---
+++ voices/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  voices
+//
+//  Created by mac on 16/9/22.
+//  Copyright © 2016年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ voices/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  voices
+//
+//  Created by mac on 16/9/22.
+//  Copyright © 2016年 mac. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
